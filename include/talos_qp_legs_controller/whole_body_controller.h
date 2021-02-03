500ffb7e06b4bc7acfd218d43d88b8a8b2883442
Ramón Calvo González <ramon@ramoncalvo.com>
2021-02-03 23:03:36 +0100

    Simple OSQP problem sample

--- include/talos_qp_legs_controller/whole_body_controller.h
+++ include/talos_qp_legs_controller/whole_body_controller.h
@@ -1,19 +1,36 @@
 #include <controller_interface/controller.h>
 #include <hardware_interface/joint_command_interface.h>
 #include <pluginlib/class_list_macros.h>
+#include <OsqpEigen/Solver.hpp>
 
 namespace whole_body_controller_ns {
 
 class WholeBodyController : public controller_interface::Controller<
-				hardware_interface::PositionJointInterface> {
-  public:
-    bool init(hardware_interface::PositionJointInterface *hw, ros::NodeHandle &n) override;
-    void update(const ros::Time &time, const ros::Duration &period) override;
-    void starting(const ros::Time &time) override;
-    void stopping(const ros::Time &time) override;
+				hardware_interface::EffortJointInterface> {
+public:
+  bool init(hardware_interface::EffortJointInterface *hw,
+	    ros::NodeHandle &n) override;
+  void update(const ros::Time &time, const ros::Duration &period) override;
+  void starting(const ros::Time &time) override;
+  void stopping(const ros::Time &time) override;
 
-  private:
-    std::vector<hardware_interface::JointHandle> joint_handles_;
-    std::vector<double> init_pos_;
+private:
+  /// Handle for every controllable joint
+  std::vector<hardware_interface::JointHandle> joint_handles_;
+  /// Store initial positions
+  std::vector<double> init_pos_;
+
+  /// QP Solver instance
+  OsqpEigen::Solver solver_;
+  /// QP Hessian matrix
+  Eigen::SparseMatrix<double> P_;
+  /// Gradient matrix
+  Eigen::VectorXd q_;
+  /// Linear constraint matrix
+  Eigen::SparseMatrix<double> A_;
+  /// Lower bound matrix
+  Eigen::VectorXd l_;
+  /// Upper bound matrix
+  Eigen::VectorXd u_;
 };
 } // namespace whole_body_controller_ns
--- src/whole_body_controller.cc
+++ src/whole_body_controller.cc
@@ -1,7 +1,10 @@
 #include <talos_qp_legs_controller/whole_body_controller.h>
+#include <eigen3/Eigen/Eigen>
+#include <OsqpEigen/OsqpEigen.h>
 
 namespace whole_body_controller_ns {
-  bool WholeBodyController::init(hardware_interface::PositionJointInterface *hw, ros::NodeHandle &nh)
+
+  bool WholeBodyController::init(hardware_interface::EffortJointInterface *hw, ros::NodeHandle &nh)
   {
     std::vector<std::string> joint_names;
 
@@ -16,6 +19,30 @@ namespace whole_body_controller_ns {
     for(size_t i = 0; i < joint_names.size(); ++i) {
       joint_handles_.push_back(hw->getHandle(joint_names.at(i)));
     }
+
+    /* --- Solver parameters --- */
+    // Set warm start
+    solver_.settings()->setWarmStart(true);
+    solver_.settings()->setAlpha(1.0);
+
+    // Set the number of variables and constraints
+    solver_.data()->setNumberOfVariables(2);
+    solver_.data()->setNumberOfConstraints(3);
+
+    /* --- QP Formulation --- */
+    P_.resize(2,2);
+    P_.insert(0, 0) = 4.; P_.insert(0, 1) = 1.;
+    P_.insert(1, 0) = 1.; P_.insert(1, 1) = 2.;
+    q_ = Eigen::VectorXd(2);
+    q_ << 1., 1.;
+    A_.resize(3,2);
+    A_.insert(0, 0) = 1.; A_.insert(0, 1) = 1.;
+    A_.insert(1, 0) = 1.; A_.insert(1, 1) = 0.;
+    A_.insert(2, 0) = 0.; A_.insert(2, 1) = 1.;
+    l_ = Eigen::VectorXd(3);
+    l_ << 1., 0., 0.;
+    u_ = Eigen::VectorXd(3);
+    u_ << 1., 0.7, 0.7;
 
     return true;
   }
@@ -39,7 +66,31 @@ namespace whole_body_controller_ns {
 
   void WholeBodyController::update(const ros::Time &time, const ros::Duration &period)
   {
-    joint_handles_[3].setCommand(50 * (1.0 - joint_handles_[3].getPosition()));
+    // Set variables and constraint matrixes
+    if (not solver_.data()->setHessianMatrix(P_)) std::runtime_error("Could not set Hessian matrix!");
+    if (not solver_.data()->setGradient(q_)) std::runtime_error("Could not set gradient matrix!");
+    if (not solver_.data()->setLinearConstraintsMatrix(A_)) std::runtime_error("Could not set linear constraint matrix!");
+    if (not solver_.data()->setLowerBound(l_)) std::runtime_error("Could not set lower bound!");
+    if (not solver_.data()->setUpperBound(u_)) std::runtime_error("Could not set upper bound!");
+
+    // Instantiate the solver_
+    if (not solver_.initSolver()) std::runtime_error("Could not instantiate solver!");
+
+    // Solve the QP problem
+    if (not solver_.solve()) std::runtime_error("Solution not found!");
+
+    // Retrieve the solution
+    Eigen::VectorXd solution;
+    solution = solver_.getSolution();
+
+    // Print solution
+    std::stringstream sol_ss;
+    sol_ss << "La matriz P es: " << std::endl << P_ << std::endl
+	   << "La matriz A es: " << std::endl << A_ << std::endl
+	   << "La solucion es: " << std::endl << solution << std::endl;
+    ROS_INFO_STREAM(sol_ss.str());
+
+    joint_handles_.at(2).setCommand(solution(1));
   }
 }
 
