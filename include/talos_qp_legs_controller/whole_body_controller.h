4904af6de4d4b6ea02f12de2e6cada36d12214f5
Ramón Calvo González <ramon@ramoncalvo.com>
2021-02-04 16:53:58 +0100

    QP controller update instead of reset

--- include/talos_qp_legs_controller/whole_body_controller.h
+++ include/talos_qp_legs_controller/whole_body_controller.h
@@ -1,3 +1,8 @@
+/**
+ *   \file whole_body_controller.h
+ *   \brief Header file for the Whole Body Controller.
+ */
+
 #include <controller_interface/controller.h>
 #include <hardware_interface/joint_command_interface.h>
 #include <pluginlib/class_list_macros.h>
@@ -8,6 +13,9 @@ namespace whole_body_controller_ns {
 class WholeBodyController : public controller_interface::Controller<
 				hardware_interface::EffortJointInterface> {
 public:
+  WholeBodyController();
+  ~WholeBodyController();
+
   bool init(hardware_interface::EffortJointInterface *hw,
 	    ros::NodeHandle &n) override;
   void update(const ros::Time &time, const ros::Duration &period) override;
@@ -15,6 +23,43 @@ public:
   void stopping(const ros::Time &time) override;
 
 private:
+  /**
+   *  Initialize the default parameters for the QP solver.
+   */
+  void setSolverParameters(void);
+  /** 
+   *  Sends the @ref P_, @ref q_, @ref A_, @ref l_ and @ref u_
+   *  matrices to the solver.
+   */
+  void setSolverData(void);
+  /** 
+   *  Updates the @ref P_, @ref q_, @ref A_, @ref l_ and @ref u_
+   *  matrices to the solver.
+   */
+  void updateSolverData(void);
+
+  /**
+   *  Update the @ref P_, @ref q_, @ref A_, @ref l_ and @ref u_
+   *  matrices according to the current robot state.
+   */
+  void updateQPFormulation(void);
+  /**
+   *  Update the @ref P_ matrix according to the robot state.
+   */
+  bool updateHessianMatrix(void);
+  /**
+   *  Update the @ref q_ matrix according to the robot state.
+   */
+  bool updateGradientMatrix(void);
+  /**
+   *  Update the @ref l_ and @ref u_ matrices according to the robot state.
+   */
+  bool updateBounds(void);
+  /**
+   *  Update the @ref A_ matrix according to the robot state.
+   */
+  bool updateLinearConstraints(void);
+  
   /// Handle for every controllable joint
   std::vector<hardware_interface::JointHandle> joint_handles_;
   /// Store initial positions
--- src/whole_body_controller.cc
+++ src/whole_body_controller.cc
@@ -4,6 +4,21 @@
 
 namespace whole_body_controller_ns {
 
+  WholeBodyController::WholeBodyController()
+  {
+    // Set matrix and variable sizes
+    P_.resize(2,2);
+    q_.resize(2);
+    A_.resize(3,2);
+    l_.resize(3);
+    u_.resize(3);
+  }
+
+  WholeBodyController::~WholeBodyController()
+  {
+
+  }
+
   bool WholeBodyController::init(hardware_interface::EffortJointInterface *hw, ros::NodeHandle &nh)
   {
     std::vector<std::string> joint_names;
@@ -20,29 +35,16 @@ namespace whole_body_controller_ns {
       joint_handles_.push_back(hw->getHandle(joint_names.at(i)));
     }
 
-    /* --- Solver parameters --- */
-    // Set warm start
-    solver_.settings()->setWarmStart(true);
-    solver_.settings()->setAlpha(1.0);
+    // Generate QP formulation matrices for initial state
+    updateQPFormulation();
 
-    // Set the number of variables and constraints
-    solver_.data()->setNumberOfVariables(2);
-    solver_.data()->setNumberOfConstraints(3);
+    // Set the QP solver default parameters
+    setSolverParameters();
+    // Initialize the matrices of the QP solver
+    setSolverData();
 
-    /* --- QP Formulation --- */
-    P_.resize(2,2);
-    P_.insert(0, 0) = 4.; P_.insert(0, 1) = 1.;
-    P_.insert(1, 0) = 1.; P_.insert(1, 1) = 2.;
-    q_ = Eigen::VectorXd(2);
-    q_ << 1., 1.;
-    A_.resize(3,2);
-    A_.insert(0, 0) = 1.; A_.insert(0, 1) = 1.;
-    A_.insert(1, 0) = 1.; A_.insert(1, 1) = 0.;
-    A_.insert(2, 0) = 0.; A_.insert(2, 1) = 1.;
-    l_ = Eigen::VectorXd(3);
-    l_ << 1., 0., 0.;
-    u_ = Eigen::VectorXd(3);
-    u_ << 1., 0.7, 0.7;
+    // Instantiate the solver
+    if (not solver_.initSolver()) std::runtime_error("Could not instantiate solver!");
 
     return true;
   }
@@ -66,15 +68,11 @@ namespace whole_body_controller_ns {
 
   void WholeBodyController::update(const ros::Time &time, const ros::Duration &period)
   {
-    // Set variables and constraint matrixes
-    if (not solver_.data()->setHessianMatrix(P_)) std::runtime_error("Could not set Hessian matrix!");
-    if (not solver_.data()->setGradient(q_)) std::runtime_error("Could not set gradient matrix!");
-    if (not solver_.data()->setLinearConstraintsMatrix(A_)) std::runtime_error("Could not set linear constraint matrix!");
-    if (not solver_.data()->setLowerBound(l_)) std::runtime_error("Could not set lower bound!");
-    if (not solver_.data()->setUpperBound(u_)) std::runtime_error("Could not set upper bound!");
+    // Update the QP formulation from the current robot state
+    updateQPFormulation();
 
-    // Instantiate the solver_
-    if (not solver_.initSolver()) std::runtime_error("Could not instantiate solver!");
+    // Send the new matrices to the QP solver
+    updateSolverData();
 
     // Solve the QP problem
     if (not solver_.solve()) std::runtime_error("Solution not found!");
@@ -92,6 +90,79 @@ namespace whole_body_controller_ns {
 
     joint_handles_.at(2).setCommand(solution(1));
   }
+
+  void WholeBodyController::setSolverParameters()
+  {
+    // Set warm start
+    solver_.settings()->setWarmStart(true);
+    solver_.settings()->setAlpha(1.0);
+
+    // Set the number of variables and constraints
+    solver_.data()->setNumberOfVariables(2);
+    solver_.data()->setNumberOfConstraints(3);
+  }
+
+  void WholeBodyController::setSolverData()
+  {
+    if (not solver_.data()->setHessianMatrix(P_)) std::runtime_error("Could not set Hessian matrix!");
+    if (not solver_.data()->setGradient(q_)) std::runtime_error("Could not set gradient matrix!");
+    if (not solver_.data()->setLinearConstraintsMatrix(A_)) std::runtime_error("Could not set linear constraint matrix!");
+    if (not solver_.data()->setLowerBound(l_)) std::runtime_error("Could not set lower bound!");
+    if (not solver_.data()->setUpperBound(u_)) std::runtime_error("Could not set upper bound!");
+  }
+
+  void WholeBodyController::updateSolverData()
+  {
+    if (not solver_.updateHessianMatrix(P_)) std::runtime_error("Could not set Hessian matrix!");
+    if (not solver_.updateGradient(q_)) std::runtime_error("Could not set gradient matrix!");
+    if (not solver_.updateLinearConstraintsMatrix(A_)) std::runtime_error("Could not set linear constraint matrix!");
+    if (not solver_.updateLowerBound(l_)) std::runtime_error("Could not set lower bound!");
+    if (not solver_.updateUpperBound(u_)) std::runtime_error("Could not set upper bound!");
+  }
+
+  bool WholeBodyController::updateHessianMatrix()
+  {
+    // TODO
+    P_.resize(2, 2);
+    P_.insert(0, 0) = 4. + std::cos(ros::Time::now().toSec()); P_.insert(0, 1) = 1.;
+    P_.insert(1, 0) = 1.; P_.insert(1, 1) = 2.;
+    return true;
+  }
+
+  bool WholeBodyController::updateGradientMatrix()
+  {
+    // TODO
+    q_ << 1., 1.;
+    return true;
+  }
+
+  bool WholeBodyController::updateBounds()
+  {
+    // TODO
+    l_ << 1., 0., 0.;
+    u_ << 1., 0.7, 0.7;
+    return true;
+  }
+
+  bool WholeBodyController::updateLinearConstraints()
+  {
+    // TODO
+    A_.resize(3, 2);
+    A_.insert(0, 0) = 1.; A_.insert(0, 1) = 1.;
+    A_.insert(1, 0) = 1.; A_.insert(1, 1) = 0.;
+    A_.insert(2, 0) = 0.; A_.insert(2, 1) = 1.;
+    return true;
+  }
+
+  void WholeBodyController::updateQPFormulation()
+  {
+    // Update the QP formulation from the current robot state
+    if (not updateHessianMatrix()) std::runtime_error("Error while calculating hessian matrix");
+    if (not updateGradientMatrix()) std::runtime_error("Error computing the gradient matrix");
+    if (not updateBounds()) std::runtime_error("Error computing the bounds");
+    if (not updateLinearConstraints()) std::runtime_error("Error computing the linear constraint matrix");
+  }
+
 }
 
 // Register the plugin
