7261e20c46d06daf63ba9e8f2c720f6ba872f87f
Ramon Calvo Gonzalez <ramon@ramoncalvo.com>
2021-07-04 23:58:45 +0200

    Docker image is working!
    * Remove a lot of unused packages
    * Install all dependencies
    
    TODO: there is surely a lot of packages that are installed and are not
    necessary.

--- run_container.sh
+++ run_container.sh
@@ -1,11 +1,11 @@
-NAME=ros_melodic_talos_wbc
+NAME=ros_talos
 
 # Create the docker image
-docker built -t $NAME
+docker build -t $NAME .
 
 # Start a terminal with mounted volumes
-docker run -it --net=host \
-    --volume="${PWD}/talos_wbc_controller":"/talos_public_workspace/src/talos_wbc_controller":rw \
-    --volume="${PWD}/xpp_talos":"/talos_public_workspace/src/xpp_talos":rw \
-    --volume="${PWD}/osqp":"/talos_public_workspace/src/osqp":rw \
-    $NAME bash # TODO change container name\
+docker run -it --rm -p=6080:80 \
+    --volume="${PWD}/talos_wbc_controller":"/home/ubuntu/talos_public_ws/src/talos_wbc_controller":rw \
+    --volume="${PWD}/xpp_talos":"/home/ubuntu/talos_public_ws/src/xpp_talos":rw \
+    --volume="${PWD}/osqp":"/home/ubuntu/talos_public_ws/src/osqp":rw \
+    $NAME bash
